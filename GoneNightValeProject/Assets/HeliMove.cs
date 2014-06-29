e23d3a52e2bd1acf679b63fcc231b867222c20f8
Seanna Musgrave <seannamusgrave@gmail.com>
2014-06-29 11:21:47 -0700

    Imported models, built city out.

---
+++ GoneNightValeProject/Assets/HeliMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeliMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speed = -0.1f;
+		transform.position += transform.forward * speed;
+	}
+}
