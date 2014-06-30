c69ca954827d0b6fb6925b689c59b576887cde23
Seanna Musgrave <seannamusgrave@gmail.com>
2014-06-29 18:36:57 -0700

    Added mission stuff

---
+++ GoneNightValeProject/Assets/Scripts/PlayerMissionTracker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMissionTracker : MonoBehaviour {
+
+	private GameObject truckMissionSite;
+	private GameObject radio;
+	private int theMission = 1;
+
+	// Use this for initialization
+	void Start () {
+		truckMissionSite = GameObject.Find ("TrucksMissionSite");
+		radio = GameObject.Find ("Radio");
+
+		Debug.Log (truckMissionSite);
+		Debug.Log (radio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (theMission == 1) {
+			if (Vector3.Distance (transform.position, truckMissionSite.transform.position) < 50f) {
+				theMission = 2;
+				Debug.Log ("At Trucks Mission");
+				radio.audio.clip = radio.GetComponent<Radio>().moveCrates;
+				radio.audio.Play ();
+
+			}
+		}
+	}
+
+}
---
+++ GoneNightValeProject/Assets/Scripts/Radio.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radio : MonoBehaviour {
+
+	public AudioClip moveCrates;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("r")) {
+			AudioSource bgMusic = GameObject.Find("Main Camera").audio;
+			GameObject.Find ("TrucksMissionMarker").renderer.enabled = true;
+			bgMusic.volume = 0.25f;
+			audio.Play ();
+			Invoke ("restoreBgMusic", 15f);
+		}
+	
+	}
+
+	void restoreBgMusic() {
+		AudioSource bgMusic = GameObject.Find("Main Camera").audio;
+		bgMusic.volume = 1;
+		GameObject.Find ("TrucksMissionMarker").renderer.enabled = false;
+	}
+}
